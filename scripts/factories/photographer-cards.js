0a45e3cfadaf3a1c70fb6c6a54b107356f7791d9
Raoul Mendy <raoul.mendy@gmail.com>
2022-02-02 09:14:22 -0600

    Allègement du code pour JS index

--- scripts/factories/photographer-cards.js
+++ scripts/factories/photographer-cards.js
@@ -1,46 +1,36 @@
 function photographerFactory(data) {
   const { name, city, tagline, price, portrait } = data;
   const picture = `assets/images/photographers/${portrait}`;
-  
+
   function getUserCardDOM() {
     // Génération des éléments HTML + Nommage
     const article = document.createElement("article");
-    const link = document.createElement("a");
-    const img = document.createElement("img");
-    const h2 = document.createElement("h2");
-    const pOne = document.createElement("p");
-    const pTwo = document.createElement("p");
-    const pThree = document.createElement("p");
-    
     article.setAttribute("class", "photographer-card");
-    link.setAttribute("class", "photographer-card__link");
-    link.setAttribute("href", "photographer.html");
-    img.setAttribute("src", picture);
-    img.setAttribute("class", "photographer-card__link__portrait");
-    h2.setAttribute("class", "photographer-card__link__name");
-    pOne.setAttribute("class", "photographer-card__city");
-    pTwo.setAttribute("class", "photographer-card__tagline");
-    pThree.setAttribute("class", "photographer-card__price");
-    
-    // Contenu des éléments HTML
-    h2.textContent = name;
-    pOne.textContent = city;
-    pTwo.textContent = tagline;
-    pThree.textContent = `${price}€/jour`;
-    
-    // Agencement des éléments HTML
-    article.appendChild(link);
-    link.appendChild(img);
-    link.appendChild(h2);
-    article.appendChild(pOne);
-    article.appendChild(pTwo);
-    article.appendChild(pThree);
 
+    const photographerCard = `
+      <a class="photographer-card__link" href="photographer.html">
+        <img
+          src="${picture}"
+          class="photographer-card__link__portrait"
+        />
+        <h2 class="photographer-card__link__name">${name}</h2>
+      </a>
+    <p class="photographer-card__city">${city}</p>
+    <p class="photographer-card__tagline">${tagline}</p>
+    <p class="photographer-card__price">${price}/jour</p>`;
+
+    // const photographerHeader = `
+    // <div>
+    //   <h1 class="">${name}</h1>
+    //   <p class="">${city}</p>
+    //   <p class="">${tagline}</p>
+    // </div>
+    // <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+    // <img src="${picture}" class="" />
+    // `;
+
+    article.innerHTML = photographerCard;
     return article;
   }
   return { name, picture, getUserCardDOM };
 }
-
-
-
-
